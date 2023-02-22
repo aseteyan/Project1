f021a98d57dbc593a65102d964fae937ebf167f8
Aseteyan <y15s17m12@gmail.com>
2023-02-22 13:35:24 +0900

    WindowsAPI Hook test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,53 @@
 #include <iostream>
+#include <Windows.h>
+#include <map>
 
-int main()
-{
+using namespace std;
 
-	system("pause>0");
+HHOOK hHook;
+map<int, int> keyDownCounters;
+map<int, int> keyUpCounters;
+
+LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
+    if (nCode == HC_ACTION) {
+        KBDLLHOOKSTRUCT* p = (KBDLLHOOKSTRUCT*)lParam;
+        if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN) {
+            keyDownCounters[p->vkCode]++;
+            //cout << "Key Down: " << p->vkCode << " Count: " << keyDownCounters[p->vkCode] << endl;
+        }
+        else if (wParam == WM_KEYUP || wParam == WM_SYSKEYUP) {
+            keyUpCounters[p->vkCode]++;
+            cout << "Key Up: " << p->vkCode << " Count: " << keyUpCounters[p->vkCode] << endl;
+
+            int total = 0;
+            for (auto i = keyUpCounters.begin(); i != keyUpCounters.end(); ++i) {
+                cout << i->first << " => " << i->second << endl;
+                total += i->second;
+            }
+            cout << "total: " << total << endl;
+
+            if (p->vkCode == 27) {
+                keyUpCounters.clear();
+                cout << "Cleared the KeyUpCounters." << endl;
+            }
+        }
+    }
+    return CallNextHookEx(hHook, nCode, wParam, lParam);
+}
+
+int main() {
+    hHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, NULL, 0);
+    if (hHook == NULL) {
+        cerr << "Failed to install hook" << endl;
+        return 1;
+    }
+
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0) != 0) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    UnhookWindowsHookEx(hHook);
+    return 0;
 }
